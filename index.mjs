5aef4ca75b5b9791b57971858cfa8c8937b6059f
Krzysztof Pająk <chris.araneo@gmail.com>
2025-05-21 23:07:20 +0200

    feat: createConfig function

---
+++ index.mjs
@@ -0,0 +1,70 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import angular from "angular-eslint";
+import eslintPluginJsonc from "eslint-plugin-jsonc";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import jsoncParser from "jsonc-eslint-parser";
+
+export default function createConfig(jsonFiles, sourceFiles, htmlFiles) {
+  return tseslint.config(
+    ...eslintPluginJsonc.configs["flat/recommended-with-jsonc"],
+    {
+      files: jsonFiles,
+      languageOptions: {
+        parser: jsoncParser,
+      },
+    },
+    {
+      files: sourceFiles,
+      extends: [
+        eslint.configs.all,
+        ...tseslint.configs.all,
+        ...angular.configs.tsAll,
+      ],
+      processor: angular.processInlineTemplates,
+      rules: {
+        "@angular-eslint/directive-selector": [
+          "error",
+          {
+            type: "attribute",
+            prefix: "app",
+            style: "camelCase",
+          },
+        ],
+        "@angular-eslint/component-selector": [
+          "error",
+          {
+            type: "element",
+            prefix: "app",
+            style: "kebab-case",
+          },
+        ],
+        "@typescript-eslint/no-extraneous-class": "off",
+      },
+    },
+    {
+      files: sourceFiles,
+      plugins: {
+        "simple-import-sort": simpleImportSort,
+      },
+      rules: {
+        "simple-import-sort/imports": "error",
+        "simple-import-sort/exports": "error",
+      },
+    },
+    {
+      files: htmlFiles,
+      extends: [...angular.configs.templateAll],
+    },
+    {
+      ignores: [
+        "node_modules/",
+        "reports/",
+        ".stryker-tmp/",
+        ".angular",
+        "package.json",
+        "package-lock.json",
+      ],
+    }
+  );
+}
