5bb2ee396b60705ed924e6f1fa87ab63877caeff
Krzysztof Pająk <chris.araneo@gmail.com>
2026-01-25 14:15:41 +0100

    fix: add missing json plugins, update dependencies

--- src/json/jsonc.ts
+++ src/json/jsonc.ts
@@ -1,3 +1,4 @@
+import json from '@eslint/json';
 import type { Linter } from 'eslint';
 import jsonc from 'eslint-plugin-jsonc';
 import jsoncParser from 'jsonc-eslint-parser';
@@ -7,6 +8,10 @@ export const getJsoncConfig = (jsons: string[]): Linter.Config => ({
   languageOptions: {
     parser: jsoncParser,
   },
+  plugins: {
+    json,
+    jsonc,
+  } as Linter.Config['plugins'],
   rules: {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     ...(jsonc.configs['flat/recommended-with-jsonc'] as any).rules,
