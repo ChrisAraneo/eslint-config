50cadbea267be8b21b8d8a265d56bf8693383170
Krzysztof Pająk <chris.araneo@gmail.com>
2026-02-07 19:14:42 +0100

    fix(get-keys): unique keys, function was returning duplicates

--- src/utils/get-keys.ts
+++ src/utils/get-keys.ts
@@ -1,4 +1,7 @@
-export const getKeys = <T extends object>(obj: T): (keyof T)[] => [
-  ...(Object.keys(obj) as (keyof T)[]),
-  ...(Reflect.ownKeys(obj) as (keyof T)[]),
-];
+import { uniq } from 'lodash-es';
+
+export const getKeys = <T extends object>(obj: T): (keyof T)[] =>
+  uniq([
+    ...(Object.keys(obj) as (keyof T)[]),
+    ...(Reflect.ownKeys(obj) as (keyof T)[]),
+  ]);
