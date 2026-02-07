88e9c18b8776c19640f8d5d351c33be0bb4501f7
Krzysztof Pająk <chris.araneo@gmail.com>
2026-02-07 18:07:59 +0100

    chore: add jest, add jest config, update tsconfig

---
+++ jest.config.ts
@@ -0,0 +1,48 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    'index.ts',
+    '!src/**/*.d.ts',
+    '!**/__tests__/**',
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  preset: 'ts-jest/presets/default-esm',
+  roots: ['<rootDir>'],
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.test.ts', '**/?(*.)+(spec|test).ts'],
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: {
+          allowUnreachableCode: false,
+          alwaysStrict: true,
+          esModuleInterop: true,
+          lib: ['es2020', 'dom'],
+          module: 'NodeNext',
+          moduleResolution: 'nodenext',
+          noImplicitAny: true,
+          noImplicitReturns: true,
+          noUncheckedIndexedAccess: true,
+          noUnusedLocals: true,
+          noUnusedParameters: true,
+          skipLibCheck: true,
+          strictNullChecks: true,
+          target: 'es2020',
+        },
+        useESM: true,
+      },
+    ],
+  },
+};
+
+export default config;
+
