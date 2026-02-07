f167ee6bbaf72b1229286471c007c4a482ce0fb3
Krzysztof Pająk <chris.araneo@gmail.com>
2026-02-08 00:35:02 +0100

    style: format code

--- src/builder/add-cross-config-off-rules.ts
+++ src/builder/add-cross-config-off-rules.ts
@@ -1,42 +1,42 @@
 import { chain } from 'lodash-es';
 
 import {
+  type ConfigBlock,
   ConfigKey,
   JSONS,
   NX,
   SOURCES,
   TEMPLATES,
   TESTS,
-  type ConfigBlock,
 } from '../interfaces.js';
-import { getConfigValue } from './get-config-value.js';
 import { appendConfigWhenDefined } from './append-config-when-defined.js';
 import { createOffRulesConfig } from './create-off-rules-config.js';
 import { findFirstFiles } from './find-first-files.js';
+import { getConfigValue } from './get-config-value.js';
 import { getConfigsToDisable } from './get-configs-to-disable.js';
 
 export const addCrossConfigOffRules = (configBlock: ConfigBlock): ConfigBlock =>
   chain(
     ((configBlock) => [
       {
-        key: SOURCES,
         configs: getConfigValue(configBlock, SOURCES),
         disableFrom: [TEMPLATES, JSONS],
+        key: SOURCES,
       },
       {
-        key: TESTS,
         configs: getConfigValue(configBlock, TESTS),
         disableFrom: [TEMPLATES, JSONS],
+        key: TESTS,
       },
       {
-        key: TEMPLATES,
         configs: getConfigValue(configBlock, TEMPLATES),
         disableFrom: [SOURCES, TESTS, JSONS, NX],
+        key: TEMPLATES,
       },
       {
-        key: JSONS,
         configs: getConfigValue(configBlock, JSONS),
         disableFrom: [SOURCES, TESTS, TEMPLATES, NX],
+        key: JSONS,
       },
     ])(configBlock),
   )
