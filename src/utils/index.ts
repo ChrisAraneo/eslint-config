d8983f043a255e0fc31d216b81fd5b28c888a740
Krzysztof Pająk <chris.araneo@gmail.com>
2026-02-07 19:15:30 +0100

    fix: update exports

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,5 +3,4 @@ export { createOffRulesPerFilePattern } from './create-off-rules-per-file-patter
 export { extractFilesPerKey } from './extract-files-per-key.js';
 export { extractRulesPerKey } from './extract-rules-per-key.js';
 export { getKeys } from './get-keys.js';
-export { isNotEmpty } from './is-not-empty.js';
 export { mapValues } from './map-values.js';
