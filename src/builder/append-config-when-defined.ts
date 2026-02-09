9edf10eae6619842478d44cdbd3e07902bef52af
Krzysztof Pająk <chris.araneo@gmail.com>
2026-02-09 23:37:00 +0100

    feat(append-config-when-defined): do not append any arrays and empty objects

--- src/builder/append-config-when-defined.ts
+++ src/builder/append-config-when-defined.ts
@@ -1,7 +1,12 @@
 import type { Linter } from 'eslint';
-import { isObject } from 'lodash-es';
+import { isArray, isObject } from 'lodash-es';
+
+import { getKeys } from './get-keys.js';
 
 export const appendConfigWhenDefined = (
   configs: Linter.Config[],
   config: Linter.Config | unknown,
-): Linter.Config[] => (isObject(config) ? [...configs, config] : configs);
+): Linter.Config[] =>
+  isObject(config) && !!getKeys(config).length && !isArray(config)
+    ? [...configs, config]
+    : [...configs];
