a520d221384fa242a182bf5608b840a7a37ba084
Krzysztof Pająk <chris.araneo@gmail.com>
2026-04-18 22:16:57 +0200

    feat(angular): extend angular configs in ng perfectionist config

--- src/angular/ng-perfectionist.ts
+++ src/angular/ng-perfectionist.ts
@@ -12,19 +12,25 @@ export const getNgPerfectionistConfigs = ({
 }: Input = {}): Linter.Config[] =>
   match(sources?.length ?? 0)
     .with(0, () => [])
-    .otherwise(() => [
-      {
-        files: sources,
-        plugins: {
-          '@chris.araneo/ng-perfectionist': ngPerfectionist,
-        },
-        processor: angular.processInlineTemplates,
-        rules: {
-          '@chris.araneo/ng-perfectionist/sort-component-imports': 'error',
-          '@chris.araneo/ng-perfectionist/sort-component-style-urls': 'error',
-          '@chris.araneo/ng-perfectionist/sort-ng-module-declarations': 'error',
-          '@chris.araneo/ng-perfectionist/sort-ng-module-exports': 'error',
-          '@chris.araneo/ng-perfectionist/sort-ng-module-imports': 'error',
-        },
-      } as Linter.Config,
-    ]);
+    .otherwise(() =>
+      angular.configs.tsAll.map(
+        (config) =>
+          ({
+            ...config,
+            files: sources,
+            plugins: {
+              ...config.plugins,
+              '@chris.araneo/ng-perfectionist': ngPerfectionist,
+            },
+            rules: {
+              '@chris.araneo/ng-perfectionist/sort-component-imports': 'error',
+              '@chris.araneo/ng-perfectionist/sort-component-style-urls':
+                'error',
+              '@chris.araneo/ng-perfectionist/sort-ng-module-declarations':
+                'error',
+              '@chris.araneo/ng-perfectionist/sort-ng-module-exports': 'error',
+              '@chris.araneo/ng-perfectionist/sort-ng-module-imports': 'error',
+            },
+          }) as Linter.Config,
+      ),
+    );
