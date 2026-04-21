66ec14198c94162be2c21e3627e907bf482b145b
Krzysztof Pająk <chris.araneo@gmail.com>
2026-04-22 00:30:16 +0200

    fix(angular): avoid duplicate plugin registration in tsAll configs

--- src/angular/angular-eslint.ts
+++ src/angular/angular-eslint.ts
@@ -14,47 +14,45 @@ export const getAngularSourcesConfigs = ({
 }: Input = {}): Linter.Config[] =>
   match(sources?.length ?? 0)
     .with(0, () => [])
-    .otherwise(() =>
-      angular.configs.tsAll.map(
+    .otherwise(() => [
+      ...angular.configs.tsAll.map(
         (config) =>
           ({
             ...config,
             files: sources,
-            plugins: {
-              ...config.plugins,
-              '@chris.araneo/ng-perfectionist': ngPerfectionist,
-            },
-            rules: {
-              ...config.rules,
-              '@angular-eslint/component-selector': [
-                'error',
-                {
-                  prefix: prefix ?? 'app',
-                  style: 'kebab-case',
-                  type: 'element',
-                },
-              ],
-              '@angular-eslint/directive-selector': [
-                'error',
-                {
-                  prefix: prefix ?? 'app',
-                  style: 'camelCase',
-                  type: 'attribute',
-                },
-              ],
-              '@angular-eslint/no-forward-ref': 'off',
-              '@angular-eslint/prefer-on-push-component-change-detection':
-                'off',
-              '@angular-eslint/prefer-output-emitter-ref': 'off',
-              '@angular-eslint/prefer-signals': 'off',
-              '@chris.araneo/ng-perfectionist/sort-component-imports': 'error',
-              '@chris.araneo/ng-perfectionist/sort-component-style-urls':
-                'error',
-              '@chris.araneo/ng-perfectionist/sort-ng-module-declarations':
-                'error',
-              '@chris.araneo/ng-perfectionist/sort-ng-module-exports': 'error',
-              '@chris.araneo/ng-perfectionist/sort-ng-module-imports': 'error',
-            },
           }) as Linter.Config,
       ),
-    );
+      {
+        files: sources,
+        plugins: {
+          '@chris.araneo/ng-perfectionist': ngPerfectionist,
+        },
+        rules: {
+          '@angular-eslint/component-selector': [
+            'error',
+            {
+              prefix: prefix ?? 'app',
+              style: 'kebab-case',
+              type: 'element',
+            },
+          ],
+          '@angular-eslint/directive-selector': [
+            'error',
+            {
+              prefix: prefix ?? 'app',
+              style: 'camelCase',
+              type: 'attribute',
+            },
+          ],
+          '@angular-eslint/no-forward-ref': 'off',
+          '@angular-eslint/prefer-on-push-component-change-detection': 'off',
+          '@angular-eslint/prefer-output-emitter-ref': 'off',
+          '@angular-eslint/prefer-signals': 'off',
+          '@chris.araneo/ng-perfectionist/sort-component-imports': 'error',
+          '@chris.araneo/ng-perfectionist/sort-component-style-urls': 'error',
+          '@chris.araneo/ng-perfectionist/sort-ng-module-declarations': 'error',
+          '@chris.araneo/ng-perfectionist/sort-ng-module-exports': 'error',
+          '@chris.araneo/ng-perfectionist/sort-ng-module-imports': 'error',
+        },
+      },
+    ]);
